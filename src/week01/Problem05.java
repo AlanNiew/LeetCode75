121cd3d6113f5ae9a6e2585d56d46bb20ad58381
SouthWind9984 <728097735@qq.com>
2024-08-04 10:20:12 +0800

    Day5，双指针

--- src/week01/Problem05.java
+++ src/week01/Problem05.java
@@ -1,7 +1,6 @@
 package week01;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,23 +10,22 @@ import java.util.Set;
  * Date 2024/8/4 9:25
  */
 public class Problem05 {
+
     public String reverseVowels(String s) {
         char[] chars = s.toCharArray();
-        int length = chars.length;
-        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
-        int [] indexArr = new int[length];
-        int len = 0;
-        for (int i = 0; i < length; i++) {
-            if (set.contains(chars[i])){
-                indexArr[len++] = i;
-            }
-        }
-        for (int i = 0; i < len / 2; i++) {
-            int last = len - i - 1;
-            char tmp = chars[indexArr[i]];
-            chars[indexArr[i]] = chars[indexArr[last]];
-            chars[indexArr[last]] = tmp;
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U'));
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right){
+            while (left < right && !set.contains(chars[left])) left++;
+            while (left < right && !set.contains(chars[right])) right--;
+            char tmp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = tmp;
+            left++;
+            right--;
         }
         return new String(chars);
     }
+
 }
