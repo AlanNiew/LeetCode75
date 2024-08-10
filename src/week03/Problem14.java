59d039ffdb4c52ae978db1f427f92305674041a4
SouthWind9984 <728097735@qq.com>
2024-08-10 21:56:56 +0800

    643. 子数组最大平均数 I ，滑动窗口 ——Day13

---
+++ src/week03/Problem14.java
@@ -0,0 +1,29 @@
+package week03;
+
+/**
+ * @author SouthWind
+ * 编程千万条，规范第一条
+ * Date 2024/8/10 21:35
+ */
+public class Problem14 {
+    /**
+     * 643. 子数组最大平均数 I
+     * k 为窗口大小
+     */
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int left = 0;
+        int right = k;
+        int max = sum;
+        while (right<nums.length){
+            sum = sum - nums[left++] + nums[right++];
+            if ( sum > max){
+                max = sum;
+            }
+        }
+        return max*1.0/k;
+    }
+}
